bbb0e20825c19936d68d7cbb1d4dc799851567f6
frobles03 <64452088+frobles03@users.noreply.github.com>
2020-05-07 20:11:25 -0400

    Add files via upload

--- src/com.company/Calculadora.java
+++ src/com.company/Calculadora.java
@@ -14,7 +14,7 @@ public class Calculadora {
         System.out.println("=========================================");
 
 
-        String[] simbolos = {"+", "-", "*", "/", " elevado a ", "¿Cual de los dos numeros es mayor? ", "Modificar Los Numeros", "Salir"};
+        String[] simbolos = { " * ", " elevado a ", " Modificar Los Numeros ", " Salir "};
         System.out.println(">>>>>>Seleccione la opcion deseada<<<<<<");
         for (int i = 0; i < simbolos.length; i++) {
             System.out.print("(" + (i + 1) + ") ");
@@ -28,31 +28,15 @@ public class Calculadora {
         }
         int opcion = (int) ingresarUnNumero("Ingrese una opcion");
         switch (opcion) {
-            case 1: //sumar
-                resultado = suma();
-                System.out.println(num1 + "+" + num2 + "=" + resultado);
-                break;
-            case 2: //resta
-                resultado = restar();
-                System.out.println(num1 + "-" + num2 + "=" + resultado);
-                break;
-            case 3: /*Multiplicacion
+
+            case 1: /*Multiplicacion
                     (Fernando Robles)
                     */
                 resultado = multiplicacion();
                 System.out.println(num1 + "*" + num2 + "=" + resultado);
                 break;
-            case 4: //Division
-                // (Lorenzo Devia Rubio)
-                // Para evitar altercados con la division por 0, se restringe su uso y se devuelve al menu
-                if (num2 != 0) {
-                    resultado = dividir();
-                    System.out.println(num1 + "/" + num2 + "=" + resultado);
-                } else {
-                    System.out.println("Error, el divisor es 0");
-                }
-                break;
-            case 5: /* Potencia
+
+            case 2: /* Potencia
                     (Fernando Robles)
                     Utilice el metodo de  "Math.pow" haciendo que la variable "num1" sea la base y "num2" el exponente.
                     */
@@ -62,63 +46,24 @@ public class Calculadora {
                     resultado=potencia();
                     System.out.println(num1 + "^" + num2 + "=" + resultado);
                     }
-
-
-
-
-
                 break;
-            case 6: //Comparacion
-                System.out.println(comparar());
-                break;
-            case 7: //Ingresar los Numeros
+
+            case 3: //Ingresar los Numeros
                 ingresarLosNumeros();
                 break;
-            case 8: //Salir
+            case 4: //Salir
                 System.out.println("Hasta la proxima!");
                 return; //Se rompe la recursividad
             default://En caso que el usuario ingrese una opcion invalida
                 System.out.println("Error, la opcion ingresada es incorrecta, intentelo Nuevamente");
                 menu();
 
-        }
-        menu();//Una vez realizada una de las operaciones (excepto salir), se ejecutara de nuevo el menu (Funcion recursiva)
-
-    }
-
-    private double suma() {
-        //(Lucas Palminio)
-        //Este metodo retorna la suma de los atributos num1 y num2
-        return num1 + num2;
-    }
-
-    private double restar() {
-        //(Lucas Palminio)
-        //Este metodo retorna la resta de los atributos num1 y num2
-        return num1 - num2;
-    }
-
-    private String comparar() {
-        // (Lorenzo Devia rubio)
-        // Primero verifica si ambos numeros son iguales, si es verdadero retorna el mensaje correspondiente en String,
-        // si es falso verifica si el primer numero es mayor que el segundo, si es verdadero retorna el mensaje correspondiente en String,
-        // finalmente si es falso se asume que el segundo numero es mayor que el primero y se retorna el mensaje correspondiente en String
-        if (num1 == num2) {
-            return "Ambos numeros son iguales";
-        } else {
-            if (num1 > num2) {
-                return num1 + " es mayor que " + num2;
-            } else {
-                return num2 + " es mayor que " + num1;
             }
+            menu();
         }
-    }
 
-    private double dividir() {
-        return num1 / num2;
-        // (Lorenzo Devia rubio)
-        //Este metodo retorna la division de los atributos de num1 entre num2
-    }
+
+
 
     private double multiplicacion(){
         /*Fernando Robles*/
